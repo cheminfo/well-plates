14f0698bb5c91f48f7d6635057b84d23a3cbcd90
Daniel Kostro <kostro.d@gmail.com>
2018-08-31 21:53:00 +0200

    rollup: export minified and non minified build configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,48 +1,49 @@
 import babel from 'rollup-plugin-babel';
 import minify from 'rollup-plugin-babel-minify';
 
-const isProd = process.env.NODE_ENV === 'production';
-
-const plugins = [];
-if (isProd) {
-  plugins.push(
-    minify({
-      comments: false
-    })
-  );
-}
-
 const output = {
   file: 'dist/well-plate.js',
   format: 'umd',
   name: 'wellPlate'
 };
 
-if (isProd) {
-  output.file = 'dist/well-plate.min.js';
-  output.sourcemap = true;
-}
-plugins.push(
-  babel({
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          targets: {
-            browsers: [
-              'chrome >= 54',
-              'last 2 edge versions',
-              'last 1 safari version'
-            ]
-          }
-        }
-      ]
-    ]
-  })
-);
+const outputMinified = Object.assign({}, output, {
+  file: 'dist/well-plate.min.js',
+  sourcemap: true
+});
 
-export default {
-  input: './lib-es6/index.js',
-  output,
-  plugins
+const minifyConfig = {
+  comments: false
 };
+
+const babelConfig = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          browsers: [
+            'chrome >= 54',
+            'last 2 edge versions',
+            'last 1 safari version'
+          ]
+        }
+      }
+    ]
+  ]
+};
+
+const inputFile = './lib-es6/index.js';
+
+export default [
+  {
+    input: inputFile,
+    output,
+    plugins: [babel(babelConfig)]
+  },
+  {
+    input: inputFile,
+    output: outputMinified,
+    plugins: [babel(babelConfig), minify(minifyConfig)]
+  }
+];
